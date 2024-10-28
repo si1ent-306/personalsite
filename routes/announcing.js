7313cc68e30eea32f2b53746a30c17b0ec7872a8
Elliott Wagner <elliottjwag@hotmail.com>
2024-10-28 13:20:30 -0600

    stuff

--- routes/announcing.js
+++ routes/announcing.js
@@ -6,5 +6,35 @@ router.get('/', function (req, res) {
     console.log(__dirname + '/../views/announcing.html');
     res.sendFile(path.join(__dirname, '../views/announcing/announcing.html'))
 });
+router.get('/overview', function (req, res) {
+    console.log(__dirname + '/../views/overview.html');
+    res.sendFile(path.join(__dirname, '../views/announcing/overview.html'))
+});
+router.get('/strengths-and-weaknesses', function (req, res) {
+    console.log(__dirname + '/../views/strengths-and-weaknesses.html');
+    res.sendFile(path.join(__dirname, '../views/announcing/strengths-and-weaknesses.html'))
+});
+router.get('/experiences', function (req, res) {
+    console.log(__dirname + '/../views/experiences.html');
+    res.sendFile(path.join(__dirname, '../views/announcing/experiences.html'))
+});
+router.get('/teamsandvenues', function (req, res) {
+    console.log(__dirname + '/../views/teamsandvenues.html');
+    res.sendFile(path.join(__dirname, '../views/announcing/teamsandvenues.html'))
+});
+router.get('/education', function (req, res) {
+    console.log(__dirname + '/../views/education.html');
+    res.sendFile(path.join(__dirname, '../views/announcing/education.html'))
+});
+
+router.get('/highlights', function (req, res) {
+    console.log(__dirname + '/../views/highlights.html');
+    res.sendFile(path.join(__dirname, '../views/announcing/highlights.html'))
+});
+
+router.get('/other', function (req, res) {
+    console.log(__dirname + '/../views/other.html');
+    res.sendFile(path.join(__dirname, '../views/announcing/other.html'))
+});
 //Export router object and require it in main js file
 module.exports = router;
